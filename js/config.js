eda10edb79d06cdfa9c61cddf2caf48120faa88b
cm5wxjmcpv-bit <cm5wxjmcpv@privaterelay.appleid.com>
2025-11-08 13:13:30 -0500

    Delete js directory

--- js/config.js
+++
@@ -1,6 +0,0 @@
-// FILL THESE AFTER YOU DEPLOY YOUR APPS SCRIPT WEB APP
-window.APP_CONFIG = {
-  WEB_APP_URL: "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec",
-  SHEET_ID: "YOUR_SHEET_ID",
-  COMPLETION_SOURCE: "github-pages-mvp"
-};
